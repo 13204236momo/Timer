bbbf581ba39976f6a23c0bf1730be34eb5168397
zhou <953605663@qq.com>
2020-06-02 16:52:52 +0800

    换肤架构集成

--- app/src/main/java/com/mo/zhou/timer/App.java
+++ app/src/main/java/com/mo/zhou/timer/App.java
@@ -2,13 +2,11 @@ package com.mo.zhou.timer;
 
 import android.app.Application;
 
-import com.example.skin_library.SkinManager;
-
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        SkinManager.init(this);
+        //SkinManager.init(this);
     }
 }
